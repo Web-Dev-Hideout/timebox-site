4a09ce36c0bcff023fcd963efd3ea9fb304c2e16
Naheen Habib <naheenhabib@outlook.com>
2022-07-31 22:12:21 +1000

    new commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Right from './components/Right';
 import Left from './components/Left'
+import Footer from './components/Footer';
 function App() {
   return (
     <div className='container'>
       <Left />
       <Right />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                <p className='footer__name'>  &copy; Time Manager 2022</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
